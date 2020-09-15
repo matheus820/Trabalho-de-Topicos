50c6dc8522d4bcff0e1f0d5cf2aa007f38fb840d
matheus <matheusavelino742@gmail.com>
2020-09-15 15:15:55 -0300

    upload

---
+++ Alunogerenciamento/src/Aluno.java
@@ -0,0 +1,161 @@
+
+public class Aluno {
+    
+private String nome;
+private String cpf;         
+private String rg;
+private int dataDenascimento;
+private String filliação;
+private String estadoCivil;
+private int numeroTelefone;
+private String email;
+private String naturalidade;
+private int cep;
+private String bairro;
+private String rua;
+private int numeroCasa;
+private String matricula;
+private String estadoDoAluno;
+private float anoDeingresso;
+private float anoDeSaida;
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public int getDataDenascimento() {
+        return dataDenascimento;
+    }
+
+    public void setDataDenascimento(int dataDenascimento) {
+        this.dataDenascimento = dataDenascimento;
+    }
+
+    public String getFilliação() {
+        return filliação;
+    }
+
+    public void setFilliação(String filliação) {
+        this.filliação = filliação;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+
+    public int getNumeroTelefone() {
+        return numeroTelefone;
+    }
+
+    public void setNumeroTelefone(int numeroTelefone) {
+        this.numeroTelefone = numeroTelefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNaturalidade() {
+        return naturalidade;
+    }
+
+    public void setNaturalidade(String naturalidade) {
+        this.naturalidade = naturalidade;
+    }
+
+    public int getCep() {
+        return cep;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getRua() {
+        return rua;
+    }
+
+    public void setRua(String rua) {
+        this.rua = rua;
+    }
+
+    public int getNumeroCasa() {
+        return numeroCasa;
+    }
+
+    public void setNumeroCasa(int numeroCasa) {
+        this.numeroCasa = numeroCasa;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getEstadoDoAluno() {
+        return estadoDoAluno;
+    }
+
+    public void setEstadoDoAluno(String estadoDoAluno) {
+        this.estadoDoAluno = estadoDoAluno;
+    }
+
+    public float getAnoDeingresso() {
+        return anoDeingresso;
+    }
+
+    public void setAnoDeingresso(float anoDeingresso) {
+        this.anoDeingresso = anoDeingresso;
+    }
+
+    public float getAnoDeSaida() {
+        return anoDeSaida;
+    }
+
+    public void setAnoDeSaida(float anoDeSaida) {
+        this.anoDeSaida = anoDeSaida;
+    }
+
+
+
+}
